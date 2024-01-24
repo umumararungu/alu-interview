73f2046a73fd7ea53bd194f5112e4e862d18ae69
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2024-01-24 21:09:58 +0200

    done

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,12 +6,12 @@ def minOperations(n):
     if n == 1:
         return 0
     
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
+    num = [float('inf')] * (n + 1)
+    num[1] = 0
     
     for i in range(2, n + 1):
         for j in range(1, i):
             if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
+                num[i] = min(num[i], num[j] + i // j)
     
-    return dp[n] if dp[n] != float('inf') else 0
+    return num[n] if num[n] != float('inf') else 0
