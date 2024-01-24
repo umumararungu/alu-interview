d6fed0ede4af3dfe7311675faa03f08d07f8a68e
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2024-01-24 21:20:17 +0200

    whitespace

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """calculating
 """
- 
+
+
 def minOperations(n):
+    """
+    oparation
+    """
     if n == 1:
         return 0
-    
+
     num1 = 0
     num2 = 2
 
-    while n> 1:
-        if n % num2 ==0:
+    while n > 1:
+        if n % num2 == 0:
             n //= num2
             num1 += num2
         else:
